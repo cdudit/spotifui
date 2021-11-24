9ed5ffd7bc24b606de7da1947adb03b009586164
Clement Dudit <clement.dudit@etudiant.univ-lr.fr>
2021-11-24 12:18:54 +0100

    Initial Commit

---
+++ SpotifUI-Lab/SpotifUI_LabApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifUI_LabApp.swift
+//  SpotifUI-Lab
+//
+//  Created by Clément Dudit on 24/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct SpotifUI_LabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
